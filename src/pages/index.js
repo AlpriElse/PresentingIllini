da5506c943617bffb1e501ebddc6817743f8c3fc
AlpriElse <alprielse@gmail.com>
2018-12-02 21:47:00 -0600

    Incorporate NextJS + Redux example into codebase

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,44 @@
-import Header from '../components/Header'
-import Layout from '../components/Layout'
-import LoginPage from '../containers/LoginPage'
+// import Header from '../components/Header'
+// import Layout from '../components/Layout'
+// import LoginPage from '../containers/LoginPage'
+//
+// const Index = () => (
+//   <div>
+//     <Header />
+//     <Layout>
+//       <LoginPage />
+//     </Layout>
+//   </div>
+// )
+//
+// export default Index
+import React from 'react'
+import {connect} from 'react-redux'
+import {startClock, serverRenderClock} from '../store'
+import Examples from '../components/examples'
 
-const Index = () => (
-  <div>
-    <Header />
-    <Layout>
-      <LoginPage />
-    </Layout>
-  </div>
-)
+class Index extends React.Component {
+  static getInitialProps ({ reduxStore, req }) {
+    const isServer = !!req
+    reduxStore.dispatch(serverRenderClock(isServer))
 
-export default Index
+    return {}
+  }
+
+  componentDidMount () {
+    const {dispatch} = this.props
+    this.timer = startClock(dispatch)
+  }
+
+  componentWillUnmount () {
+    clearInterval(this.timer)
+  }
+
+  render () {
+    return (
+      <Examples />
+    )
+  }
+}
+
+export default connect()(Index)
